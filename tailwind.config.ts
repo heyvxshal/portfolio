075c0005422baccd0726d6f2da869020874a62c3
Vishal patil <46629074+vishalpatilweb@users.noreply.github.com>
2024-06-02 01:39:43 +0530

    Initial Setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        black: {
+          DEFAULT: "#000",
+        },
+        primary: {
+          DEFAULT: "#DBF64F",
+        },
+        highlight: {
+          DEFAULT: "#1230D7",
+        },
+        accent: {
+          DEFAULT: "#FBFCFA",
+        },
       },
     },
   },
